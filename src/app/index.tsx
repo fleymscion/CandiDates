5533c18ece03244d2ff6d4536034da8d12e2aff4
Cipriano <hilaricipriano@gmail.com>
2026-09-25 20:37:46 +0800

    Insert Comments here

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Testinggg brrreee</Text>
+      <Text>Testingeee brrreee</Text>
     </View>
   );
 }
