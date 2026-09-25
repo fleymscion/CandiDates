07662133fe8418b2178468a91ea19e1333291484
Cipriano <hilaricipriano@gmail.com>
2026-09-25 20:34:20 +0800

    Testing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit Test</Text>
+      <Text>Testinggg brrreee</Text>
     </View>
   );
 }
